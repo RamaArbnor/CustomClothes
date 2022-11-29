4b1db79d2b7431e5186d85cbcc6514998cf30103
RamaArbnor <arama@york.citycollege.eu>
2022-11-29 19:51:00 +0200

    initial idea created Very Simple

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [size, setSize] = useState(72);
+  const [color, setColor] = useState('#000000');
+
+  let colorStyle = {color: color};
+  let sizeStyle = {'font-size': `${size}px`};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='left border'>
+        <input type='range' min="12" max="72" onChange={e => setSize(e.target.value)} ></input>
+        <input type='color' onChange={e => setColor(e.target.value)}></input>
+        {/* <input type='text'></input> */}
+      </div>
+
+      <div className='right border'>
+        <img src='https://render.fineartamerica.com/images/images-clothing-body-styles/35-9.png' />
+        <div className=' view'>
+            <p  id='viewText' contentEditable='true' style={{...sizeStyle , ...colorStyle}}>Text 1</p>
+        </div>
+      </div>
     </div>
   );
 }
